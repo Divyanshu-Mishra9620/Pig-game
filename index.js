5a356d3befb057fcde8162904bb2e26b9377588f
Divyanshu@018 <143180195+student-dv@users.noreply.github.com>
2024-05-11 12:58:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,21 +13,11 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// //starting conditions
-// score0El.textContent = 0;
-// score1El.textContent = 0;
-// diceEl.classList.add("hidden");
-
-// const scores = [0, 0]; // score of player0 will be at position 0 & for player1 at position1
-// let currScore = 0;
-// let activePlayer = 0;
-// let playing = true;
-
 let playing, scores, activePlayer, currScore;
 
 //starting conditions
 const init = function () {
-  const scores = [0, 0]; // score of player0 will be at position 0 & for player1 at position1
+  scores = [0, 0];
   currScore = 0;
   activePlayer = 0;
   playing = true;
@@ -106,5 +96,5 @@ btnHold.addEventListener("click", function () {
 });
 
 btnNew.addEventListener("click", function () {
-  init;
+  init();
 });
